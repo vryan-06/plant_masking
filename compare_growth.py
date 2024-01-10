547b7046e3a9c9920abd2a61a1c077f4c2f703ee
V Ryan <ryan19vt@gmail.com>
2024-01-10 13:34:54 +0530

    recent updates

--- compare_growth.py
+++ compare_growth.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread("./collected_data/p1.jpeg")
-img2 = cv2.imread("./collected_data/p1(2).jpeg")
-
+img1 = cv2.imread("./masked_collected_data/p2._mask.jpg")
+img2 = cv2.imread("./masked_collected_data/p2(2).._mask.jpg")
 img2 = cv2.resize(img2,(img1.shape[1], img1.shape[0]))
 
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -19,14 +18,14 @@ def mse(img1, img2):
 
 mse_val, diff = mse(img1, img2)
 
-if(mse_val>10):
+if(mse_val>0.5):
    print("There has been significant growth detected in the plant")
 else:
    print("There is no significant growth in the plant")
 # cv2.imshow("difference", diff)
-# resized_image = cv2.resize(diff, (300, 300))
-# cv2.imshow("Resized Image", resized_image)
-# cv2.imshow("1",img1)
-# # cv2.imshow("2",img2)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+resized_image = cv2.resize(diff, (300, 500))
+cv2.imshow("Difference", resized_image)
+cv2.imshow("1",cv2.resize(img1,(300,500)))
+cv2.imshow("2",cv2.resize(img2,(300,500)))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
